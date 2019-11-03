358f4d4c045654bd35790eb4a5dde017907fbc97
jbride <jbride2001@yahoo.com>
2019-11-03 15:45:11 -0700

    utils/set_env_vars.sh

--- utils/set_env_vars.sh
+++ utils/set_env_vars.sh
@@ -11,6 +11,9 @@ source $HOME/.bashrc
 
 
 
+echo "export OCP_USER=user$LAB_ID" >> $HOME/.bashrc
+echo "export OCP_ADMIN=admin$LAB_ID" >> $HOME/.bashrc
 echo "export RHSM_CONTROL_PLANE_NS=admin$LAB_ID-istio-system" >> $HOME/.bashrc
+echo "export LAB_MASTER_API=https://api.cluster-$REGION-$REGION.$SUBDOMAIN_BASE:6443" >> $HOME/.bashrc
 
 source $HOME/.bashrc
