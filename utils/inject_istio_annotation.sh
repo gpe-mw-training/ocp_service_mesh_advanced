a41de495faec1d52c114841b8c18a9c708044c0e
jbride <jbride2001@yahoo.com>
2019-12-01 22:42:52 -0700

    inject_istio_annotation.sh:   removing postgresql databases

--- utils/inject_istio_annotation.sh
+++ utils/inject_istio_annotation.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+ER_DeploymentConfigs="
+        $ERDEMO_USER-incident-service \
+        $ERDEMO_USER-incident-priority-service \
+        $ERDEMO_USER-mission-service \
+        $ERDEMO_USER-responder-service \
+        $ERDEMO_USER-process-service \
+        $ERDEMO_USER-process-viewer \
+        $ERDEMO_USER-disaster-simulator \
+        $ERDEMO_USER-emergency-console \
+        $ERDEMO_USER-responder-simulator"
+                      
+
 # Responsible for injecting the istio annotation that opts in a DC for auto injection of the envoy sidecar
 function injectAndResume() {
 
@@ -23,7 +35,7 @@ spec:
   counter=1
   while (( $replicas != $readyReplicas && $counter != 20 ))
   do
-    sleep 15 
+    sleep 1 
     oc get dc $DC_NAME -o json -n $ERDEMO_USER-er-demo > /tmp/$DC_NAME.json
     replicas=$(cat /tmp/$DC_NAME.json | jq .status.replicas)
     readyReplicas=$(cat /tmp/$DC_NAME.json | jq .status.readyReplicas)
@@ -32,16 +44,8 @@ spec:
   done
 }
 
-
-# Enable ER-Demo databases for Envoy auto-injection
-for DC_NAME in postgresql $ERDEMO_USER-process-service-postgresql
-do
-  injectAndResume
-done
-
-
-# Enable remainder of ER-Demo DeploymentConfigs for Envoy auto-injection
-for DC_NAME in $(oc get dc -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' -n $ERDEMO_USER-er-demo ) 
+# Enable ER-Demo DeploymentConfigs for Envoy auto-injection
+for DC_NAME in $ER_DeploymentConfigs;
 do
   injectAndResume
 done
